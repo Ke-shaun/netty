a4831a8195371c3024e4bde736f556f732be7b04
wuqx <wuqx@seer-group.com>
2024-05-30 17:21:49 +0800

    11

--- common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
+++ common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
@@ -102,6 +102,7 @@ public abstract class MultithreadEventExecutorGroup extends AbstractEventExecuto
                             // Let the caller handle the interruption.
                             Thread.currentThread().interrupt();
                             break;
+
                         }
                     }
                 }
